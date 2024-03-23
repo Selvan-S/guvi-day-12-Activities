1324747be02f2f7a7685f0bdbe409f6ed3688f42
Selvan-S <selvan0023@gmail.com>
2024-03-23 23:10:31 +0530

    star added

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,4 +1,12 @@
 const surveyForm = document.querySelector("#surveyForm");
 surveyForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  surveyForm.reset();
 });
+
+const starSpan = document.querySelector("#experience");
+
+for (let index = 0; index < 5; index++) {
+  const starSvg = '<img src="star-solid.svg" width="24" height="24"/>';
+  starSpan.innerHTML += starSvg + " ";
+}
